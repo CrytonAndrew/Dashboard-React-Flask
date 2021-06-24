16ac7321541b992f3400b1bc6181704e6d684eff
Cryton Andrew <crytonadrew21@gmail.com>
2021-06-25 01:09:47 +0200

    Clean up code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -13,23 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          The current time is {currentTime}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
