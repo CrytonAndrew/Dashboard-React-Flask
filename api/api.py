49cc75749fac4105df1b3eef3215417e6882f2a4
Cryton Andrew <crytonadrew21@gmail.com>
2021-06-25 01:00:37 +0200

    Update proxy

--- api/api.py
+++ api/api.py
@@ -2,7 +2,12 @@ import time
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
+def home():
+	return "<p>Hello World</p>"
+
+@app.route("/time")
 def get_current_time():					
 	return {"time": time.time()}
 
